d85540c3e4dd5ce61f21906f828a493120803a52
raymondjaramillo <rjaramillo155@cnm.edu>
2020-02-26 16:49:45 -0700

    Created Fragments and linked to qod.

--- app/src/main/java/edu/cnm/deepdive/quoteclient/controller/HomeFragment.java
+++ app/src/main/java/edu/cnm/deepdive/quoteclient/controller/HomeFragment.java
@@ -20,6 +20,7 @@ public class HomeFragment extends Fragment {
       ViewGroup container, Bundle savedInstanceState) {
     View root = inflater.inflate(R.layout.fragment_home, container, false);
     quoteText = root.findViewById(R.id.quote_text);
+    root.setOnClickListener((view) -> viewModel.refresh());
     return root;
   }
 
@@ -27,5 +28,7 @@ public class HomeFragment extends Fragment {
   public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
     viewModel = new ViewModelProvider(this).get(HomeViewModel.class);
+    viewModel.getQuote().observe(getViewLifecycleOwner(), (quote) ->
+        quoteText.setText(quote.getText()));
   }
 }
--- app/src/main/java/edu/cnm/deepdive/quoteclient/controller/HomeViewModel.java
+++ app/src/main/java/edu/cnm/deepdive/quoteclient/controller/HomeViewModel.java
@@ -4,19 +4,39 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 import edu.cnm.deepdive.quoteclient.model.Quote;
+import edu.cnm.deepdive.quoteclient.service.QuoteRepository;
+import io.reactivex.disposables.CompositeDisposable;
 
 public class HomeViewModel extends ViewModel {
 
-  private MutableLiveData<Quote> quote = new MutableLiveData<>();
+  private MutableLiveData<Quote> quote;
+  private final MutableLiveData<Throwable> throwable;
+  private final QuoteRepository repository;
+  private CompositeDisposable pending;
 
   public HomeViewModel() {
+    repository = QuoteRepository.getInstance();
+    pending = new CompositeDisposable();
+    quote = new MutableLiveData<>();
+    throwable = new MutableLiveData<>();
+    refresh();
   }
 
- public LiveData<Quote> getQuote() {
+  public LiveData<Quote> getQuote() {
     return quote;
- }
+  }
 
- public void refresh() {
-    // TODO Invoke method in repository to get new quote
- }
+  public LiveData<Throwable> getThrowable() {
+    return throwable;
+  }
+
+  public void refresh() {
+    pending.add(
+        repository.getRandom()
+            .subscribe(
+                quote::postValue,
+                throwable::postValue
+            )
+    );
+  }
 }
